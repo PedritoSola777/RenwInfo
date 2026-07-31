4f9c039178ba498f14b3e380d8f2e10a45087c92
PedritoSola777 <siteWeb2450@gmail.com>
2026-07-31 14:11:49 -0600

    fEJ2

--- assets/JS/bot.js
+++ assets/JS/bot.js
@@ -49,9 +49,13 @@ function addWelcomeMessage() {
 
 function addOptionButtons() {
     const options = [
+        "Créditos Congelados",
         "Obtener un crédito",
         "Conocer tu ahorro",
         "Consultar el saldo de tu crédito",
+        "Conocer soluciones de pago",
+        "Liberar tu hipoteca",
+        "Si un derechohabiente fallece ¿Qué debo hacer?",
         "Solicitar un retiro",
         "Consultar mi retiro",
     ];
@@ -99,7 +103,22 @@ function handleOption(option) {
 
     addUserMessage(option);
 
-    if (option === "Obtener un crédito") {
+    if (option === "Créditos Congelados") {
+        addBotMessage(`
+            <p>
+            Si originaste tu crédito hipotecario antes de mayo 2021, INFONAVIT Solución Integral mejora las condiciones de tu crédito. Y si cumples con los requisitos, <b>¡podríamos ayudarte liquidando tu deuda!</b>
+            </p>
+            <p>El programa te ofrece al menos alguno de los siguientes beneficios:</p>
+            <ul>
+                <li><b>Pagos fijos y en pesos</b>, para que sepas cuánto pagar cada mes.</li>
+                <li><b>Tasa de interés justa</b>, determinada según tu nivel de ingreso.</li>
+                <li><b>Reducción del monto de tu deuda</b>.</li>
+            </ul>
+            <p>
+            La aplicación del programa es automática si cumples con los requisitos, revisa en la sección de Saldos y Movimientos en <a href="https://micuenta.infonavit.org.mx">Mi Cuenta Infonavit</a> si ya recibiste los beneficios. Asegúrate de tener tu correo electrónico y teléfono de contacto actualizados en Mi Cuenta Infonavit, para que podamos mantenerte informado.
+            </p>
+        `);
+    } else if (option === "Obtener un crédito") {
         addBotMessage(`
             <b>Para obtener un crédito Infonavit,</b> debes tener en cuenta la variedad de productos que tenemos para ti, de acuerdo a tus necesidades como:
             <ul>
@@ -130,7 +149,38 @@ function handleOption(option) {
     } else if(option === "Consultar el saldo de tu crédito") {
         addBotMessage(`
             <p>
-            Puedes<b>revisar tu estado de cuenta</b> y descargarlo, solo debes ingresar a <a href="https://micuenta.infonavit.org.mx">Mi Cuenta Infonavit</a> en la sección "Mi crédito"
+            Puedes <b>revisar tu estado de cuenta</b> y descargarlo, solo debes ingresar a <a href="https://micuenta.infonavit.org.mx">Mi Cuenta Infonavit</a> en la sección "Mi crédito"
+            </p>
+        `);
+    } else if (option === "Conocer soluciones de pago") {
+        addBotMessage(`
+            <p>Las <b>soluciones de pago</b> que el Infonavit tiene para ti son 📢:</p>
+            <ol>
+                <li>Flexipago. 👛</li>
+                <li>Registrar el Paro técnico de tu empleador. 📅</li>
+                <li>Nivela tu pago. 📄</li>
+                <li>Estudio socioeconómico. 📋</li>
+                <li>Solución a tu medida. ✏️</li>
+                <li>Borrón y cuenta nueva por convenio privado. 🤝</li>
+                <li>Apoyo a jubilados. 👴👵</li>
+                <li>INFONAVIT Solución Integral 👨‍👩‍👧‍👦👨‍👩‍👧‍👦</li>
+            </ol>
+        `);
+        addBotMessage(`
+            <p>
+            Para <b>más información</b> sobre estos apoyos que ofrece Infonavit y cómo tramitarlos, ingresa en <a href="https://micuenta.infonavit.org.mx">este enlace</a>, en la sección "Mis ingresos disminuyeron".
+            </p>
+        `);
+    } else if (option === "Liberar tu hipoteca") {
+        addBotMessage(`
+            <p>
+            Una vez liquidado <b>tu crédito</b> continua con el proceso para liberar tu hipoteca, ingresa o regístrate en <a href="https://micuenta.infonavit.org.mx">Mi Cuenta Infonavit</a> y solicita la <i>Carta de instrucción de cancelación de hipoteca</i>, en: Mi crédito y Carta de instrucción de cancelación de hipoteca. Recuerda que el proceso concluye hasta que el RPP (Registro Público de la Propiedad) o el Notario te entrega el documento que confirma que tu crédito está libre de gravamen. Por lo que es importante que una vez que iniciaste el trámite con estas instituciones, tendrás que dar seguimiento a tu proceso de liberación de escrituras con el RPP o el notario seleccionado.
+            </p>
+        `);
+    } else if (option === "Si un derechohabiente fallece ¿Qué debo hacer?") {
+        addBotMessage(`
+            <p>
+            En caso de que el <b>titular de la cuenta Infonavit haya fallecido</b>, sus beneficiarios reconocidos ante el IMSS o quienes cuenten con la designación emitida por los Tribunales Federales en materia laboral pueden solicitar la devolución del saldo de su Subcuenta de Vivienda.
             </p>
         `);
     } else if(option === "Solicitar un retiro"){
